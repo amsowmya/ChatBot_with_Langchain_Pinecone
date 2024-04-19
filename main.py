ffd69d6e347edefa78db8f352aaf03103f979474
Sowmya AM <2260927@cognizant.com>
2024-04-19 17:32:23 +0530

    added langchain chatbot code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from langchain_community.chat_models import AzureChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferWindowMemory
@@ -11,3 +13,69 @@ import streamlit as st
 from streamlit_chat import message
 from utils import *
 
+load_dotenv()
+
+# os.environ['AZURE_OPENAI_API_KEY'] = os.getenv('AZURE_OPENAI_API_KEY')
+
+AZURE_OPENAI_API_KEY = os.environ['AZURE_OPENAI_API_KEY'] = os.getenv('AZURE_OPENAI_API_KEY')
+
+st.title("Smart Chatbot")
+
+if 'responses' not in st.session_state:
+    st.session_state['responses'] = ['How can I assist you']
+
+if 'requests' not in st.session_state:
+    st.session_state['requests'] = []
+
+llm = AzureChatOpenAI(
+    api_version="2024-02-15-preview",
+    azure_deployment="atttestgpt35turbo",
+    azure_endpoint="https://cb-att-openai-instance.openai.azure.com/",
+    api_key=AZURE_OPENAI_API_KEY
+)
+
+if 'buffer_memory' not in st.session_state:
+    st.session_state.buffer_memory = ConversationBufferWindowMemory(k=3, return_messages=True)
+
+system_msg_template = SystemMessagePromptTemplate.from_template(template="""Answer the question
+as truthfully as possing using the provided context.
+And if the answer is not contained within the context below. Say, I don't know""")
+
+human_msg_template = HumanMessagePromptTemplate.from_template(template='{input}')
+
+prompt_template = ChatPromptTemplate.from_messages([system_msg_template, 
+                                                    MessagesPlaceholder(variable_name='history'),
+                                                    human_msg_template])
+
+conversation = ConversationChain(memory=st.session_state.buffer_memory,
+                                 llm = llm,
+                                 prompt=prompt_template,
+                                 verbose=True)
+
+# container for chat history
+response_container = st.container()
+
+# container for text box
+text_container = st.container()
+
+with text_container:
+    query = st.text_input("Query: ", key="input")
+    if query:
+        with st.spinner("typing..."):
+            conversation_string = get_conversation_string()
+            st.code(conversation_string)
+            refined_query = query_refiner(conversation_string, query)
+            st.subheader("Refined Query:")
+            st.write(refined_query)
+            context = find_match(refined_query)
+            # print(context)
+            response = conversation.run(input=f"Context:\n {context} \n\nQuery: {query}")
+        st.session_state.requests.append(query)
+        st.session_state.responses.append(response)
+
+with response_container:
+    if st.session_state['responses']:
+        for i in range(len(st.session_state['responses'])):
+            message(st.session_state['responses'][i], key=str(i))
+            if i < len(st.session_state['requests']):
+                message(st.session_state['requests'][i], is_user=True, key=str(i)+"_user")
---
+++ utils.py
@@ -0,0 +1,73 @@
+from langchain_community.embeddings import AzureOpenAIEmbeddings
+from pinecone import Pinecone
+from langchain_pinecone import PineconeVectorStore
+import streamlit as st
+import os
+from openai import AzureOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# PINECONE_API_KEY = os.environ['PINECONE_API_KEY'] = os.getenv("PINECONE_API_KEY")
+# AZURE_OPENAI_API_KEY = os.environ['AZURE_OPENAI_API_KEY'] = os.getenv('AZURE_OPENAI_API_KEY')
+
+# PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+# AZURE_OPENAI_API_KEY = os.getenv('AZURE_OPENAI_API_KEY')
+
+os.environ['PINECONE_API_KEY'] = os.getenv("PINECONE_API_KEY")
+os.environ['AZURE_OPENAI_API_KEY'] = os.getenv('AZURE_OPENAI_API_KEY')
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+AZURE_OPENAI_API_KEY = os.getenv('AZURE_OPENAI_API_KEY')
+
+print(PINECONE_API_KEY)
+
+embeddings = AzureOpenAIEmbeddings(
+    deployment='vector-search-instance2',
+    model='text-embedding-ada-002',
+    openai_api_type='azure',
+    azure_endpoint='https://cb-att-openai-instance.openai.azure.com/',
+    api_key=AZURE_OPENAI_API_KEY,
+)
+
+pc = Pinecone(
+    api_key=PINECONE_API_KEY,
+    environment = 'us-east-1'
+)
+
+index = pc.Index('langchain-chatbot')
+# index = 'langchain-chatbot'
+print("========================", index)
+
+def find_match(input):
+    input_embed = embeddings.embed_query(input)
+    result = index.query(vector=input_embed, top_k=2, include_metadata=True)
+    return result['matches'][0]['metadata']['text'] + "\n" + result['matches'][1]['metadata']['text']
+
+def query_refiner(conversation, query):
+    client = AzureOpenAI(
+        api_key = AZURE_OPENAI_API_KEY,  
+        api_version = "2023-09-15-preview",
+        azure_deployment="atttestgpt35turbo",
+        azure_endpoint = 'https://cb-att-openai-instance.openai.azure.com/'
+    )
+ 
+    response = client.chat.completions.create(
+        model="gpt-35-turbo", # model = "deployment_name".
+        messages=[
+            {"role": "system", "content": "You are a helpfull assistant"},
+            {"role": "user", "content": f"Given the following user query and conversation log, formulate a question that \
+            would be the most relevant to provide the user with an answer from the knowledge base. \
+                \n\n CONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:"}
+        ]
+    )
+
+    return response.choices[0].message.content
+
+def get_conversation_string():
+    conversation_string = ""
+    for i in range(len(st.session_state['responses'])-1):
+        conversation_string += "Human: " + st.session_state['requests'][i] + "\n"
+        conversation_string += "Bot: " + st.session_state['responses'][i+1] + "\n"
+
+    return conversation_string
